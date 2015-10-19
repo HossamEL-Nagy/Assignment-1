a958222891fdc5dd13c9fecbad2819da2cc8eb7f
HossamEL-Nagy <s.hossam.f@gmail.com>
2015-10-19 21:19:03 +0200

    Create BankAccount.h

---
+++ BankAccount.h
@@ -0,0 +1,16 @@
+#include <string>
+
+using namespace std;
+
+class BankAccount
+{
+	private:
+		string Name;
+		string accountNumber;
+		double balance;
+	public:
+		BankAccount(string name, string number, double balance);
+		void deposite(double amount);
+		void withdraw(double amount);
+		void Displaly();
+};
